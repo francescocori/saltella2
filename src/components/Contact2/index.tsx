27d39bbbd3b5559e749c66324791d18a504ad832
Francesco <francesco.coriolano@gmail.com>
2025-09-04 12:08:15 +0100

    improve footer

--- src/components/Contact2/index.tsx
+++ src/components/Contact2/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Contact2() {
   return (
     <section id="contact" className="py-20 bg-brand-dark">
@@ -23,21 +25,14 @@ export default function Contact2() {
                 rel="noopener noreferrer"
                 className="flex items-center gap-3 hover:opacity-90"
               >
-                <div className="w-10 h-10 bg-brand-darker rounded-full flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-custom-text"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
+                <div className=" rounded-full flex items-center justify-center">
+                  <Image
+                    src="/Videos/Images/whatsapp.png"
+                    alt="WhatsApp"
+                    width={50}
+                    height={50}
+                    className="w-8 h-8"
+                  />
                 </div>
                 <span className="font-founders font-light text-custom-text">
                   +351 920 416 301
@@ -45,29 +40,16 @@ export default function Contact2() {
               </a>
 
               <div className="flex items-center gap-3">
-                <div className="w-10 h-10 bg-brand-darker rounded-full flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-primary-content"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
+                <div className=" rounded-full flex items-center justify-center">
+                  <Image
+                    src="/Videos/Images/map.png"
+                    alt="WhatsApp"
+                    width={50}
+                    height={50}
+                    className="w-8 h-8"
+                  />
                 </div>
-                <span className="text-custom-textSecondary font-founders font-light">
+                <span className="text-custom-textSecondary font-founders font-light mt-2">
                   Lisbon
                 </span>
               </div>
