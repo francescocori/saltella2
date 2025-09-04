34bfdb8adfbdbc9cba084d6c49b0680e8df044a0
Francesco <francesco.coriolano@gmail.com>
2025-09-04 20:26:31 +0100

    improve contact form style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
         <link
           rel="preload"
           as="image"
-          href="/Videos/Images/delivery2.jpg"
+          href="/Videos/Images/delivery3.jpg"
           type="image/jpep"
         />
         <link
